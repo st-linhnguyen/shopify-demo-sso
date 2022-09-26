332be6cb0f4627b2eac4676da01d052c61429f60
st-linhnnguyen <linh.nguyen@supremetech.vn>
2022-09-26 17:08:55 +0700

    debuging

--- assets/line-login.js
+++ assets/line-login.js
@@ -3,6 +3,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const buttonLine = document.getElementsByClassName('sso-login-line')[0];
   buttonLine.addEventListener('click', function() {
-    location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657504427&redirect_uri=https://demo-sso.myshopify.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz'
+    // location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657504427&redirect_uri=https://demo-sso.myshopify.com/account/login&state=12345abcde&scope=profile%20openid';
+
+    const csrf = document.cookie.split('; ').find(row => row.startsWith('csrfToken')).split('=')[1];
+    console.log(csrf);
   });
 });
