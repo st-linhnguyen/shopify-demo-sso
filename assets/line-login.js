757f81d783bf31f03d3bd923b77213eb19deb430
st-linhnnguyen <linh.nguyen@supremetech.vn>
2022-09-26 16:56:52 +0700

    implement login with Line

---
+++ assets/line-login.js
@@ -0,0 +1,8 @@
+// https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttonLine = document.getElementsByClassName('sso-login-line')[0];
+  buttonLine.addEventListener('click', function() {
+    location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz'
+  });
+});
