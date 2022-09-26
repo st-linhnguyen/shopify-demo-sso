77c5d221c45e25129cffcc76b5430a49aac4681c
st-linhnnguyen <linh.nguyen@supremetech.vn>
2022-09-26 17:10:55 +0700

    remove unused code

--- assets/line-login.js
+++ assets/line-login.js
@@ -3,9 +3,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const buttonLine = document.getElementsByClassName('sso-login-line')[0];
   buttonLine.addEventListener('click', function() {
-    // location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657504427&redirect_uri=https://demo-sso.myshopify.com/account/login&state=12345abcde&scope=profile%20openid';
-
-    const csrf = document.cookie.split('; ').find(row => row.startsWith('csrfToken')).split('=')[1];
-    console.log(csrf);
+    location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657504427&redirect_uri=https://demo-sso.myshopify.com/account/login&scope=profile%20openid';
   });
 });
