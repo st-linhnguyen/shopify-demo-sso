3c3fd5ef7a09b78fa58327d7c6231f52299809ad
st-linhnnguyen <linh.nguyen@supremetech.vn>
2022-09-26 14:44:20 +0700

    handle login with facebook

---
+++ assets/sso-login.js
@@ -0,0 +1,46 @@
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  if (response.status === 'connected') {
+    testAPI();
+  } else {
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.';
+  }
+}
+
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '833394284494920',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v15.0'
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonFB = document.getElementsByClassName('sso-login-facebook')[0];
+  buttonFB.addEventListener('click', () => {
+    console.log(123123);
+    checkLoginState();
+  });
+});
